2bdf138d31ee757fb6d36bfc2d58c0c1e1fedd0a
sabin dangi <sabin4.dangi@gmail.com>
2023-02-05 20:37:07 -0800

    initial commit

---
+++ src/test/java/com/sabindangi/springbootdemo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabindangi.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
